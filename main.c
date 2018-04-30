ba22fd049f7a57d90f1f54b267acedc55c1b1cae
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-04-30 22:16:38 +0100

    printa só uma vez + projeto em C

--- main.c
+++ main.c
@@ -3,16 +3,18 @@
   Daniel Santos Fernandes - 86400
  *-------------------------------*/
 
-#include <iostream>
 #include <stdio.h>
-#include <queue>
+#include <stdlib.h>
 
 #define MAX_INT 2147483647
 
+struct node;
 struct vertix;
 struct edge;
 int edmondsKarp();
 void BFS();
+struct vertix *pop();
+void push(struct vertix *vertix);
 
 typedef struct node {
 	struct vertix *vertix;
@@ -22,68 +24,48 @@ typedef struct node {
 typedef node *Node;
 
 typedef struct vertix {
-public:
     int l, c;
     int capacity;
     struct edge **adj;
-    struct vertix *pi = NULL;
-    bool isBackGround = false;
+    struct vertix *pi;
+    int isBackGround;
 } Vertix;
 
 typedef struct edge {
-public:
     Vertix *a, *b;
     int weight;
-    int flow = 0;
+    int flow;
 } Edge;
 
-class Graph {  /* l=0,c=0 - start ; l=0,c=1 - target*/
-public:
+typedef struct graph {  /* l=0,c=0 - start ; l=0,c=1 - target*/
     int M, N;
     Node start;
     Node target;
-    Graph() {
-        start = new node();
-        target = new node();
-
-        /*inicializar start*/
-        start->vertix = new Vertix();
-        start->vertix->l = 0;
-        start->vertix->c = 0;
-        start->vertix->pi = start->vertix;
-
-        /*inicializar target*/
-        target->vertix = new Vertix();
-        target->vertix->l = 0;
-        target->vertix->c = 1;
-
-        start->next = target;
-        target->prev = start;
-
-    }
-
-    void initializeVertices() {
-        Node aux;
-        start->vertix->adj = new Edge*[M*N];
-        target->vertix->adj = new Edge*[M*N];  //DESNECESSARIO
-        for (int i = 1; i <= M; i++) {
-            for (int j = 1; j <= N; j++) {
-                aux = target->prev;
-                target->prev = new node();
-                target->prev->next = target;
-                target->prev->prev = aux;
-                aux->next = target->prev;
-
-                target->prev->vertix = new Vertix();
-                target->prev->vertix->l = i;
-                target->prev->vertix->c = j;
-                target->prev->vertix->adj = new Edge*[5](); /*0-target; 1-north; 2-east; 3-south; 4-west*/
-            }
-        }
-    }
-};
+} Graph;
 
 Graph *graph;
+Node Qhead, Qtail;
+
+void initializeVertices() {
+    int i, j;
+    Node aux, start = graph->start, target = graph->target;
+    start->vertix->adj = (Edge**)malloc(sizeof(Edge*) * graph->M * graph->N);
+    for (i = 1; i <= graph->M; i++) {
+        for (j = 1; j <= graph->N; j++) {
+            aux = target->prev;
+            target->prev = (Node)malloc(sizeof(node));
+            target->prev->next = target;
+            target->prev->prev = aux;
+            aux->next = target->prev;
+
+            target->prev->vertix = (Vertix*)malloc(sizeof(Vertix));
+            target->prev->vertix->l = i;
+            target->prev->vertix->c = j;
+            target->prev->vertix->isBackGround = 0;
+            target->prev->vertix->adj = (Edge**)malloc(sizeof(Edge*) * 5); /*0-target; 1-north; 2-east; 3-south; 4-west*/
+        }
+    }
+}
 
 
 int main() {
@@ -92,9 +74,24 @@ int main() {
     Node n, above = NULL, aux;
     Edge *auxEdge;
 
-    graph = new Graph();
+    graph = (Graph*)malloc(sizeof(graph));
 
-    Vertix *s = graph->start->vertix;
+    graph->start = (Node)malloc(sizeof(node));
+    graph->target = (Node)malloc(sizeof(node));
+
+    /*inicializar start*/
+    graph->start->vertix = (Vertix*)malloc(sizeof(Vertix));
+    graph->start->vertix->l = 0;
+    graph->start->vertix->c = 0;
+    graph->start->vertix->pi = graph->start->vertix;
+
+    /*inicializar target*/
+    graph->target->vertix = (Vertix*)malloc(sizeof(Vertix));
+    graph->target->vertix->l = 0;
+    graph->target->vertix->c = 1;
+
+    graph->start->next = graph->target;
+    graph->target->prev = graph->start;
 
     /*scan de M e N*/
 	if(1 != scanf("%d", &(graph->M))) return 1;
@@ -102,36 +99,31 @@ int main() {
     
     n = graph->start;
 
-    graph->initializeVertices();
+    initializeVertices();
 
     /*scan de valores de primeiro plano*/
     for (i = 1; i <= graph->M; i++) {
         for (j = 1; j <= graph->N; j++) {
             n = n->next;
-            auxEdge = s->adj[(i-1)*graph->N + (j-1)] = new Edge();
-            auxEdge->a = s;
+            auxEdge = graph->start->vertix->adj[(i-1)*graph->N + (j-1)] = (Edge*)malloc(sizeof(Edge));
+            auxEdge->a = graph->start->vertix;
             auxEdge->b = n->vertix;
+            auxEdge->flow = 0;
             if(1 != scanf("%d", &(auxEdge->weight))) return 1;
         }
     }
 
     n = graph->start;
-    //Edge *e1;
 
     /*scan de valores de cenario*/
     for (i = 1; i <= graph->M; i++) {
         for (j = 1; j <= graph->N; j++) {
             n = n->next;
-            auxEdge = n->vertix->adj[0] = new Edge();
+            auxEdge = n->vertix->adj[0] = (Edge*)malloc(sizeof(Edge));
             auxEdge->a = n->vertix;
             auxEdge->b = graph->target->vertix;
+            auxEdge->flow = 0;
             if(1 != scanf("%d", &(auxEdge->weight))) return 1;
-
-            
-            //e1 = s->adj[(i - 1) * graph->N + (j - 1)];
-            //int capacity = (e1->weight - auxEdge->weight) > 0 ? auxEdge->weight : e1->weight;
-            //e1->flow = auxEdge->flow = capacity;
-
         }
     }
 
@@ -148,9 +140,10 @@ int main() {
             n = n->next;
             if (i > 1) {
                 above = above->next;
-                n->vertix->adj[1] = new Edge();
+                n->vertix->adj[1] = (Edge*)malloc(sizeof(Edge));
                 n->vertix->adj[1]->a = n->vertix;
                 n->vertix->adj[1]->b = above->vertix;
+                n->vertix->adj[1]->flow = 0;
                 above->vertix->adj[3] = n->vertix->adj[1];
             } /*else {
                 n->vertix->adj[1] = NULL;
@@ -161,9 +154,10 @@ int main() {
             }*/
 
             if (j < graph->N) {
-                n->vertix->adj[2] = new Edge();
+                n->vertix->adj[2] = (Edge*)malloc(sizeof(Edge));
                 n->vertix->adj[2]->a = n->vertix;
                 n->vertix->adj[2]->b = n->next->vertix;
+                n->vertix->adj[2]->flow = 0;
             }/* else {
                 n->vertix->adj[2] = NULL;
             }*/
@@ -198,27 +192,23 @@ int main() {
         }
     }
 
-    n = graph->start; //std::cout << "oi\n";
+    n = graph->start;
 
-    printf("%d\n\n", edmondsKarp()); //std::cout << "oioi\n";
+    printf("%d\n\n", edmondsKarp()); 
     
-    BFS(); //std::cout << "oioioi\n";
-
-    std::string str = "";
+    BFS();
 
     for (i = 1; i <= graph->M; i++) {
         for (j = 1; j <= graph->N; j++) {
             n = n->next;
             if (n->vertix->isBackGround) {
-                str += "C ";
+                printf("C ");
             } else {
-                str += "P ";
+                printf("P ");
             }
         }
-        str += "\n";
+        printf("\n");
     }
-
-    printf("%s", str.c_str());
 
 
    return 0;
@@ -233,66 +223,59 @@ int edmondsKarp() {
     Edge *e;
     int i, adjN, MN = graph->M * graph->N;
 
-    while (true) { //std::cout << "236---------------\n";
-        bool stop = false;
-        std::queue<Vertix*> queue;
+    while (1) {
+        int stop = 0;
+        Qhead = Qtail = NULL;
         for (n = graph->start; n != NULL ; n = n->next) {
             n->vertix->pi = NULL;
-            n->vertix->capacity = 0;
         }
         s->capacity = MAX_INT;
         s->pi = s;
-        queue.push(s);
-        while (!queue.empty()) { //std::cout << "\t239\n";
-            stop = false;
-            u = queue.front();
-            queue.pop();
+        push(s);
+        while (NULL != Qhead) { /*printf("bom meio dia");*/
+            stop = 0;
+            u = pop();
             if (0 == u->c) {
                 adjN = MN;
             } else {
                 adjN = 5;
             }
-            for (i = 0; i < adjN && !stop; i++) { //std::cout << "\t\t247 " << u->l << " " << u->c << std::endl;
+            for (i = 0; i < adjN && !stop; i++) { /*printf("boa tarde");*/
                 e = u->adj[i];
                 if (NULL == e) continue;
-                v = ( e->a == u ? e->b : e->a ); //std::cout << "\t\t "<< v->l << " " << v->c << std::endl;
-                // There is available capacity,
-                // and v is not seen before in search
+                v = ( e->a == u ? e->b : e->a );
                 if (e->weight - e->flow > 0 && v->pi == NULL) {
                     v->pi = u; 
-                    //std::cout << u->capacity << " - (" << e->weight << " - " << e->flow << ") > 0 ? " << e->weight << " - " << e->flow << " : " << u->capacity << std::endl;
                     v->capacity = (u->capacity - (e->weight - e->flow) > 0 ? e->weight - e->flow : u->capacity);
-                    //std::cout << v->capacity << std::endl;
                     if (v != t)
-                        queue.push(v);
+                        push(v);
                     else {
-                        // Backtrack search, and write flow
-                        while (v->pi != v) { //std::cout << "\t\t\t259 ";
+                        while (v->pi != v) { 
                             u = v->pi;
-                            if (0 == v->l) { //std::cout << "\t\tto target\n";
+                            if (0 == v->l) {
                                 e = u->adj[0];
-                            } else if (0 == u->l) { //std::cout << "\t\tfrom start\n";
+                            } else if (0 == u->l) { 
                                 e = u->adj[(v->l - 1) * graph->N + (v->c - 1)];
-                            } else if (u->l > v->l) { //std::cout << "\t\tto north\n";
+                            } else if (u->l > v->l) { 
                                 e = u->adj[1];
-                            } else if (u->l < v->l) { //std::cout << "\t\tto south\n";
+                            } else if (u->l < v->l) { 
                                 e = u->adj[3];
-                            } else if (u->c > v->c) { //std::cout << "\t\tto west\n";
+                            } else if (u->c > v->c) { 
                                 e = u->adj[4];
-                            } else if (u->c < v->c) { //std::cout << "\t\tto east\n";
+                            } else if (u->c < v->c) { 
                                 e = u->adj[2];
-                            } else break;//printf("WHAT THE FUCK");
+                            } else printf("WHAT THE FUCK");
                             v = ( e->a == u ? e->b : e->a );
-                            e->flow += t->capacity;  //std::cout << "\t\t\t273 " << t->capacity << std::endl;
+                            e->flow += t->capacity;
                             /*F[v][u] -= M[t]; let's ignore this line bECAUSE I CAN*/
                             v = u;
                         }
-                        stop = true;
+                        stop = 1;
                     }
                 }
             }
         }
-        if (t->pi == NULL) { // We did not find a path to t
+        if (t->pi == NULL) {
             int sum = 0;
             for (i = 0; i < graph->M * graph->N; i++) {
                 sum += s->adj[i]->flow;
@@ -310,17 +293,16 @@ void BFS() {
     Edge *e;
     int i, adjN;
 
-    std::queue<Vertix*> queue;
+    Qhead = Qtail = NULL;
 
     for (n = graph->start; n != NULL ; n = n->next) {
         n->vertix->pi = NULL;
     }
     s->pi = s;
 
-    queue.push(s);
-    while (!queue.empty()) {
-        u = queue.front();
-        queue.pop();
+    push(s);
+    while (NULL != Qhead) {
+        u = pop(Qhead, Qtail);
         if (0 == u->c) {
             adjN = graph->M * graph->N;
         } else {
@@ -332,12 +314,40 @@ void BFS() {
             v = ( e->a == u ? e->b : e->a );
             if (e->weight - e->flow > 0 && v->pi == NULL) {
                 v->pi = u;
-                v->isBackGround = true;
+                v->isBackGround = 1;
                 if (v != t)
-                    queue.push(v);
+                    push(v);
             }
         }
     }
 
 
+}
+
+void push(Vertix *vertix) {
+	Node n = (Node)malloc(sizeof(node));
+	n->vertix = vertix;
+	n->next = NULL;
+	if (Qhead == NULL){
+		Qhead = n;
+        Qtail = n;
+	} else {
+		Qtail->next = n;
+        Qtail = Qtail->next;
+	}
+}
+
+Vertix *pop() {
+	Vertix *popped;
+	if (Qhead == NULL){
+		return NULL;
+	} else {
+		popped = Qhead->vertix;
+		Node next = Qhead->next;
+		free(Qhead);
+		Qhead = next;
+		if (Qhead == NULL)
+			Qtail = NULL;
+	}
+	return popped;
 }
